1ff71a450552d2b62739f4309d06d544808d9a72
Sergey <sergey.developer.it@gmail.com>
2020-12-21 14:57:45 +0000

    creamos archivo json con lista de objetos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,20 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
-  clientes: Cliente[] = [
-    { id: 1, nombre: 'Andrés', apellido: 'Guzmán', email: 'ag@gmail.com', createdAt: '2019-07-19' },
-    { id: 2, nombre: 'Andrés', apellido: 'Gutierrez', email: 'ag2@gmail.com', createdAt: '2019-07-19' },
-    { id: 3, nombre: 'Luis', apellido: 'Guzmán', email: 'lg@gmail.com', createdAt: '2019-07-19' },
-    { id: 4, nombre: 'Pablo', apellido: 'Guzmán', email: 'pg@gmail.com', createdAt: '2019-07-19' },
-    { id: 5, nombre: 'Fernando', apellido: 'Cárcel', email: 'fc@gmail.com', createdAt: '2019-07-18' },
-    { id: 6, nombre: 'Andrés', apellido: 'Guzmán', email: 'ag@gmail.com', createdAt: '2019-07-19' },
-    { id: 7, nombre: 'Rasmus', apellido: 'Cárcel', email: 'rc@gmail.com', createdAt: '2019-07-19' }
-  ];
+  clientes: Cliente[] = [];
   constructor() { }
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Andrés', apellido: 'Guzmán', email: 'ag@gmail.com', createdAt: '2019-07-19' },
+  { id: 2, nombre: 'Andrés', apellido: 'Gutierrez', email: 'ag2@gmail.com', createdAt: '2019-07-19' },
+  { id: 3, nombre: 'Luis', apellido: 'Guzmán', email: 'lg@gmail.com', createdAt: '2019-07-19' },
+  { id: 4, nombre: 'Pablo', apellido: 'Guzmán', email: 'pg@gmail.com', createdAt: '2019-07-19' },
+  { id: 5, nombre: 'Fernando', apellido: 'Cárcel', email: 'fc@gmail.com', createdAt: '2019-07-18' },
+  { id: 6, nombre: 'Andrés', apellido: 'Guzmán', email: 'ag@gmail.com', createdAt: '2019-07-19' },
+  { id: 7, nombre: 'Rasmus', apellido: 'Cárcel', email: 'rc@gmail.com', createdAt: '2019-07-19' }
+];
