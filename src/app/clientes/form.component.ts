5a4ab7d8b47132b050875488bb9d07ec243a345b
Sergey <sergey.developer.it@gmail.com>
2021-01-10 14:35:20 +0000

    creado método create

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -6,7 +6,12 @@ import { Cliente} from './cliente';
 })
 export class FormComponent implements OnInit {
   private cliente: Cliente = new Cliente();
+  private titulo:string = "Crear Cliente";
   constructor() { }
   ngOnInit() {
+  }
+  public create(): void {
+    console.log('clicked!');
+    console.log(this.cliente);
   }
 }
