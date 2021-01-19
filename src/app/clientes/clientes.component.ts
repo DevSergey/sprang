c20c8e160d9ee1a7de6ec673ccdf67b078b72f76
Sergey <sergey.developer.it@gmail.com>
2021-01-19 00:15:03 +0000

    delete completo, crud completo con angular y spring-boot de backend

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
@@ -13,5 +14,29 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+  delete(cliente: Cliente): void {
+    swal.fire({
+      title: 'Estás seguro de borrar este cliente?',
+      text: `No podrás volver a recuperar a ${cliente.nombre} ${cliente.apellido}!`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: `Si, elimina a ${cliente.nombre} ${cliente.apellido}!`
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli =>cli !== cliente)
+            swal.fire(
+              'Borrado!',
+              'Tu cliente ha sido eliminado.',
+              'success'
+            )
+          }
+        )
+      }
+    })
   }
 }
