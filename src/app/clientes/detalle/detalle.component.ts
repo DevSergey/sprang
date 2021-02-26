204dfa794d7d04fdfb56043f944c2b551ed4772a
Sergey <sergey.developer.it@gmail.com>
2021-02-26 14:17:07 +0000

    validación de imagen antes de subir

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -27,11 +27,19 @@ export class DetalleComponent implements OnInit {
   seleccionarFoto(event){
       this.fotoSeleccionada = event.target.files[0];
       console.log(this.fotoSeleccionada);
+      if(this.fotoSeleccionada.type.indexOf('image') < 0){
+        swal.fire('Error Selección:', 'Debe ser de tipo foto', 'error');
+        this.fotoSeleccionada = null;
+      }
   }
   subirFoto(){
+    if(!this.fotoSeleccionada){
+      swal.fire('Error Upload:', 'Debe seleccionar una foto', 'error');
+    }else{
     this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id).subscribe(cliente => {
         this.cliente = cliente;
         swal.fire('La foto se ha subido correctamente!', `La foto se ha subido correctamente: ${this.cliente.foto}`, 'success');
       });
+    }
   }
 }
