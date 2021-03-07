d7fd6edf835548cba1bd50c0b95cc4bc07cdc674
Sergey <sergey.developer.it@gmail.com>
2021-03-07 20:56:47 +0000

    fase acabada, lo siguiente empezaremos con roles, auntentificación ...

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Cliente} from './cliente';
-import { Region} from './region';
-import { ClienteService} from './cliente.service';
-import { Router, ActivatedRoute} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Cliente} from './cliente';
+import {Region} from './region';
+import {ClienteService} from './cliente.service';
+import {Router, ActivatedRoute} from '@angular/router';
 import swal from 'sweetalert2';
 @Component({
   selector: 'app-form',
@@ -13,27 +13,29 @@ export class FormComponent implements OnInit {
   private titulo: string = 'Crear Cliente';
   private errores: string[];
   regiones: Region[];
-  constructor(private clienteService: ClienteService, private router: Router, private activateRoute: ActivatedRoute) { }
+  constructor(private clienteService: ClienteService, private router: Router, private activateRoute: ActivatedRoute) {
+  }
   ngOnInit() {
     this.cargarCliente();
   }
   cargarCliente(): void {
     this.activateRoute.params.subscribe(
       params => {
-        let id = params['id']
+        let id = params['id'];
         if (id) {
-          this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente);
+          this.clienteService.getCliente(id).subscribe((cliente) => this.cliente = cliente);
         }
       });
     this.clienteService.getRegiones().subscribe(regiones => this.regiones = regiones);
   }
   create(): void {
+    console.log(this.cliente);
     this.clienteService.create(this.cliente)
       .subscribe(cliente => {
-        console.log(cliente);
-        this.router.navigate(['/clientes'])
-        swal.fire('Nuevo Cliente', `El cliente ${cliente.nombre}  ${cliente.apellido} ha sido creado con éxito.`, 'success');
-      },
+          console.log(cliente);
+          this.router.navigate(['/clientes']);
+          swal.fire('Nuevo Cliente', `El cliente ${cliente.nombre}  ${cliente.apellido} ha sido creado con éxito.`, 'success');
+        },
         err => {
           this.errores = err.error.errors as string[];
           console.error(err.error.errors);
@@ -41,17 +43,24 @@ export class FormComponent implements OnInit {
         });
   }
   update(): void {
+    console.log(this.cliente);
     this.clienteService.update(this.cliente)
       .subscribe(json => {
-        console.log(json.mensaje);
-        console.log(json.cliente);
-        this.router.navigate(['/clientes']);
-        swal.fire('Cliente Actualizado', `${json.mensaje}: ${json.cliente.nombre}  ${json.cliente.apellido}`, 'success');
-      },
+          console.log(json.mensaje);
+          console.log(json.cliente);
+          this.router.navigate(['/clientes']);
+          swal.fire('Cliente Actualizado', `${json.mensaje}: ${json.cliente.nombre}  ${json.cliente.apellido}`, 'success');
+        },
         err => {
           this.errores = err.error.errors as string[];
           console.error(err.error.errors);
           console.error('Código del error desde el backend: ' + err.status);
         });
+  }
+  compararRegion(o1: Region, o2: Region): boolean {
+    if (o1 === undefined && o2 === undefined) {
+      return true;
+    }
+    return o1 === null || o2 === null ? false : o1.id == o2.id;
   }
 }
