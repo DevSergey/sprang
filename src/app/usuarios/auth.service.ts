18414f69ff54651a5efccf9389db072fccea8c39
Sergey <sergey.developer.it@gmail.com>
2021-09-06 17:42:02 +0000

    login 1.0

---
+++ src/app/usuarios/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Usuario} from './usuario';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+  login(usuario: Usuario): Observable<any> {
+    const urlEndpoint = 'http:
+    const credenciales = btoa('appspring5angular' + ':' + '12345');
+    const httpHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded','Authorization': 'Basic ' + credenciales});
+    let params = new URLSearchParams();
+    params.set('grant_type', 'password');
+    params.set('username', usuario.username);
+    params.set('password', usuario.password);
+    console.log(params.toString());
+    return this.http.post<any>(urlEndpoint, params.toString(), { headers: httpHeaders });
+  }
+}
--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from './usuario';
 import swal from 'sweetalert2';
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
@@ -8,16 +10,25 @@ import swal from 'sweetalert2';
 export class LoginComponent implements OnInit {
   titulo: string = 'Por favor Loguéate';
   usuario: Usuario;
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
     this.usuario = new Usuario();
   }
   ngOnInit() {
   }
-  login(): void {
-    console.log(this.usuario);
-    if(this.usuario.username == null || this.usuario.password == null) {
-      swal.fire('Error Login', 'Username o password vacíos!', 'error');
+    login(): void {
+      console.log(this.usuario);
+    if (this.usuario.username == null || this.usuario.password == null) {
+      swal.fire('Error Login', 'Username o password vacías!', 'error');
       return;
     }
+    this.authService.login(this.usuario).subscribe(response => {
+      console.log(response);
+      this.router.navigate(['/clientes']);
+      swal.fire('Login', 'Hola ' + this.usuario.username + ', has iniciado sesión con éxito!', 'success');
+    }, err => {
+      if (err.status === 400) {
+        swal.fire('Error Login', 'Usuario o clave incorrectas!', 'error');
+      }
+    });
   }
 }
