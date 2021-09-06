00f94b0a7564e6dd2839f243fbfe25bea3d0b570
Sergey <sergey.developer.it@gmail.com>
2021-09-06 12:57:40 +0000

    creamos clase usuario, modelamos formulario para la clase y añadimos método login

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-  titulo:string = 'Por favor Sing In';
-  constructor() { }
+  titulo: string = 'Por favor Loguéate';
+  usuario: Usuario;
+  constructor() {
+    this.usuario = new Usuario();
+  }
   ngOnInit() {
+  }
+  login(): void {
+    console.log(this.usuario);
+    if(this.usuario.username == null || this.usuario.password == null) {
+      swal.fire('Error Login', 'Username o password vacíos!', 'error');
+      return;
+    }
   }
 }
